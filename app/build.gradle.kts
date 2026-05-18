91d1bd1debc958d43d2caf3bd040ad648701c945
KevshuppD <kevshuppd@gmail.com>
2026-05-17 23:48:01 -0400

    chore: bump version to 1.2.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "calendario.kevshupp.diariokevinali"
         minSdk = 24
         targetSdk = 35
-        versionCode = 25
-        versionName = "1.2.7"
+        versionCode = 26
+        versionName = "1.2.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
