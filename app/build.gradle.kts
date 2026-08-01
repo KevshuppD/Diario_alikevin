f506d6e6f68d877706ec5f750a3e7b61f20967c9
KevshuppD <kevinshupp@gmail.com>
2026-08-01 11:46:38 -0400

    Build: subir version a 1.7.26 (Code 71) para lanzamiento de tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "calendario.kevshupp.diariokevinali"
         minSdk = 24
         targetSdk = 35
-        versionCode = 70
-        versionName = "1.7.25"
+        versionCode = 71
+        versionName = "1.7.26"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
