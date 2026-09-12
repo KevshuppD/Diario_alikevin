3071c30101a9a81e8551bab1caa505b591576b2b
KevshuppD <kevinshupp@gmail.com>
2026-09-12 11:49:19 -0300

    chore(release): bump version to 1.7.49 (versionCode 94) - optimizacion de PackageInstaller

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "calendario.kevshupp.diariokevinali"
         minSdk = 24
         targetSdk = 35
-        versionCode = 93
-        versionName = "1.7.48"
+        versionCode = 94
+        versionName = "1.7.49"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
