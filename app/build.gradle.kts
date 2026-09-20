3eb833d497008bf70d0cca9c65c3a95e600ce82c
KevshuppD <kevinshupp@gmail.com>
2026-09-19 22:55:41 -0300

    chore(release): bump version to 1.7.64 (Build 109)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "calendario.kevshupp.diariokevinali"
         minSdk = 24
         targetSdk = 35
-        versionCode = 108
-        versionName = "1.7.63"
+        versionCode = 109
+        versionName = "1.7.64"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
